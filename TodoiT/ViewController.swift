2466de33d93757bab2fd32b136281fc4437c7148
lzhangivy <lzhang.ivy@gmail.com>
2019-09-20 20:34:38 -0400

    Initial Commit

---
+++ TodoiT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoiT
+//
+//  Created by Lijing Zhang on 9/20/19.
+//  Copyright © 2019 Lijing Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
